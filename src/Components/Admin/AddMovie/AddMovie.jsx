eb98036982833261e88072c243eae01a2463475a
Айдар Кадырбеков <aidarkadyrbekov@MacBook-Pro-Ajdar.local>
2022-06-06 19:39:42 +0600

    input

--- src/Components/Admin/AddMovie/AddMovie.jsx
+++ src/Components/Admin/AddMovie/AddMovie.jsx
@@ -1,74 +1,55 @@
-import { Button, TextField } from "@mui/material";
 import React from "react";
-import { ToastContainer } from "react-bootstrap";
+import {
+  Button,
+  Form,
+  FormControl,
+  InputGroup,
+  ToastContainer,
+} from "react-bootstrap";
 import "./AddMovie.css";
 
 const AddProduct = () => {
   return (
     <div className="addProduct__container">
-      <div className="inputs">
-        <div>
-          <TextField
-            id="filled-basic"
-            label="Название фильма"
-            variant="filled"
-            name="title"
-            className="inp1"
-          />
-        </div>
-        <div>
-          <TextField
-            id="filled-basic"
-            label="Жанр"
-            variant="filled"
-            name="type"
-            className="inp2"
-          />
-        </div>
-        <div>
-          <TextField
-            id="filled-basic"
-            label="Описание"
-            variant="filled"
-            name="description"
-            className="inp3"
-          />
-        </div>
-        <div>
-          <TextField
-            id="filled-basic"
-            type="number"
-            label="Цена"
-            variant="filled"
-            name="price"
-            className="inp4"
-          />
-        </div>
-        <div>
-          <TextField
-            id="filled-basic"
-            label="Постер"
-            variant="filled"
-            name="img"
-            className="inp5"
-          />
-        </div>
-        <div>
-          <Button type="button" variant="contained" className="btnSave">
-            Добавить фильм
-          </Button>
-          <ToastContainer
-            position="top-center"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-        </div>
+      <div className="containerAddTopic">
+        <Form.Control
+          className="inp"
+          size="lg"
+          type="text"
+          placeholder="Название фильма"
+        />
+        <Form.Control
+          className="inp"
+          size="lg"
+          type="text"
+          placeholder="Жанр"
+        />
+        <Form.Control
+          className="inp"
+          size="lg"
+          type="text"
+          placeholder="Описание"
+        />
+        <Form.Control
+          className="inp"
+          size="lg"
+          type="text"
+          placeholder="Цена"
+        />
+        <Form.Control
+          className="inp"
+          size="lg"
+          type="text"
+          placeholder="Постер"
+        />
+        <button className="addTopic-btn">Добавить</button>
+        <ToastContainer
+          position="top-center"
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+        />
       </div>
     </div>
   );
